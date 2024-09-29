a98c9115d139d2bc810dfecf0e80275d90d4b11e
Nana <evylaveras@gmail.com>
2024-09-29 20:36:32 -0300

    Optimized layout for tablet

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,11 +7,11 @@ import { Cart } from "../components/cart/cart";
 export const Home = () => {
   return (
     <div className="home_content">
-      <p className="home-paragrafo">Desserts</p>
       <div className="home-container">
+        <p className="home-paragrafo">Desserts</p>
         <Card />
-        <Cart />
       </div>
+        <Cart />
     </div>
   );
 };
